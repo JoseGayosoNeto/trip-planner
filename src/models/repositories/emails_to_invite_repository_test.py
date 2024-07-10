049b39a806c8392f7f7e76b0d3b7389b32cb464c
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-07-10 09:02:28 -0300

    test: add pytest skip decorator in emails_to_invite tests

--- src/models/repositories/emails_to_invite_repository_test.py
+++ src/models/repositories/emails_to_invite_repository_test.py
@@ -1,3 +1,4 @@
+import pytest
 import uuid
 from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
 from src.models.settings.db_connection_handler import db_connection_handler
@@ -6,6 +7,7 @@ from src.models.settings.db_connection_handler import db_connection_handler
 db_connection_handler.connect()
 trip_id = str(uuid.uuid4())
 
+@pytest.mark.skip(reason='Test with interation with db')
 def test_registry_email():
     conn = db_connection_handler.get_connection()
     emails_to_invite_repository = EmailsToInviteRepository(conn)
@@ -18,6 +20,7 @@ def test_registry_email():
     
     emails_to_invite_repository.registry_email(email_infos)
 
+@pytest.mark.skip(reason='Test with interation with db')
 def test_find_emails_from_trip():
     conn = db_connection_handler.get_connection()
     emails_to_invite_repository = EmailsToInviteRepository(conn)
