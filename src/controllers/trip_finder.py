a697c9f91bd7341d16f3da396cb8778271de569b
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-07-11 08:54:22 -0300

    feat: add implementation of trips controllers

---
+++ src/controllers/trip_finder.py
@@ -0,0 +1,30 @@
+from typing import Dict
+from src.models.repositories.trips_repository import TripsRepository
+
+class TripFinder:
+    
+    def __init__(self, trips_repository: TripsRepository) -> None:
+        self.__trips_repository = trips_repository
+
+    def find_trip_details(self, trip_id: str) -> Dict:
+        try:
+            trip = self.__trips_repository.find_trip_by_id(trip_id)
+            if not trip:
+                raise ValueError("Trip not found")
+
+            return {
+                "body": {
+                    "id": trip[0],
+                    "destination": trip[1],
+                    "starts_at": trip[2],
+                    "ends_at": trip[3],
+                    "status": trip[6]
+                },
+                "status_code": 200,
+            }
+        except Exception as e:
+            return {
+                "body": {"error": "Bad Request", "message": str(e)},
+                "status_code": 400,
+            }
+        
--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -1,8 +1,58 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 
+# Import Controllers
+from src.controllers.trip_creator import TripCreator
+from src.controllers.trip_finder import TripFinder
+from src.controllers.trip_confirmer import TripConfirmer
+from src.controllers.link_creator import LinkCreator
+
+# Import Repositories
+from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
+from src.models.repositories.trips_repository import TripsRepository
+from src.models.repositories.links_repository import LinksRepository
+
+# Import Connection
+from src.models.settings.db_connection_handler import db_connection_handler
 
 trips_routes_bp = Blueprint("trip_routes", __name__)
 
 @trips_routes_bp.route('/trips', methods=['POST'])
 def create_trip():
-    return jsonify({"route": "test"}), 200
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    emails_repository = EmailsToInviteRepository(conn)
+    controller = TripCreator(trips_repository, emails_repository)
+    
+    response = controller.create(request.json)
+    
+    return jsonify(response['body']), response['status_code']
+
+@trips_routes_bp.route('/trips/<trip_id>', methods=['GET'])
+def find_trip(trip_id):
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    controller = TripFinder(trips_repository)
+    
+    response = controller.find_trip_details(trip_id)
+    
+    return jsonify(response['body']), response['status_code']
+
+@trips_routes_bp.route('/trips/<trip_id>/confirm', methods=['PATCH'])
+def confirm_trip(trip_id):
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    controller = TripConfirmer(trips_repository)
+    
+    response = controller.confirm(trip_id)
+    
+    return jsonify(response['body']), response['status_code']
+
+@trips_routes_bp.route('/trips/<trip_id>/link/create', methods=['POST'])
+def create_link(trip_id):
+    conn = db_connection_handler.get_connection()
+    link_repository = LinksRepository(conn)
+    controller = LinkCreator(link_repository)
+    
+    response = controller.create(request.json, trip_id)
+    
+    return jsonify(response['body']), response['status_code']
